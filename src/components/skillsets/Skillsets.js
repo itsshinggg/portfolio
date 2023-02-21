3431526fd75f30e076a0a5e3e9a7b7a52d2c2e2c
shingggk <kiseshingo@cityuniversity.edu>
2023-02-20 17:48:09 -0800

    add skillsets and works

--- src/components/skillsets/Skillsets.js
+++ src/components/skillsets/Skillsets.js
@@ -1,7 +1,116 @@
 import './Skillsets.css';
+import './Skillsets.css';
+import { BsPatchCheckFill } from 'react-icons/bs';
 
 const Skillsets = () => {
-  return <section id='skillsets'></section>;
+  return (
+    <section id='skillsets'>
+      <h5>My skillsets</h5>
+      <h2>My skils</h2>
+
+      <div className='container experience__container'>
+        <div className='experience__frontend'>
+          <h3>Frontend Development</h3>
+          <div className='experience__content'>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon' />
+              <div>
+                <h4>HTML</h4>
+                <small className='text-light'>Familiar</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon' />
+              <div>
+                <h4>HTML</h4>
+                <small className='text-light'>Familiar</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon' />
+              <div>
+                <h4>HTML</h4>
+                <small className='text-light'>Familiar</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon' />
+              <div>
+                <h4>HTML</h4>
+                <small className='text-light'>Familiar</small>
+              </div>
+            </article>
+          </div>
+        </div>
+        <div className='experience__backend'>
+          <h3>Frontend Development</h3>
+          <div className='experience__content'>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon' />
+              <div>
+                <h4>HTML</h4>
+                <small className='text-light'>Familiar</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon' />
+              <div>
+                <h4>HTML</h4>
+                <small className='text-light'>Familiar</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon' />
+              <div>
+                <h4>HTML</h4>
+                <small className='text-light'>Familiar</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon' />
+              <div>
+                <h4>HTML</h4>
+                <small className='text-light'>Familiar</small>
+              </div>
+            </article>
+          </div>
+        </div>
+        <div className='experience__backend'>
+          <h3>Frontend Development</h3>
+          <div className='experience__content'>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon' />
+              <div>
+                <h4>HTML</h4>
+                <small className='text-light'>Familiar</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon' />
+              <div>
+                <h4>HTML</h4>
+                <small className='text-light'>Familiar</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon' />
+              <div>
+                <h4>HTML</h4>
+                <small className='text-light'>Familiar</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon' />
+              <div>
+                <h4>HTML</h4>
+                <small className='text-light'>Familiar</small>
+              </div>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Skillsets;
--- src/components/works/Works.js
+++ src/components/works/Works.js
@@ -1,7 +1,106 @@
 import './Works.css';
+import PortfolioImg from '../../assets/download.jpeg';
 
 const Works = () => {
-  return <section id='works'></section>;
+  return (
+    <section id='works'>
+      <h5>My Works!</h5>
+      <h2>Portfolio</h2>
+
+      <div className='container portfolio__container'>
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'>
+            <img src={PortfolioImg} alt='' />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className='portfolio__item-cta'>
+            <a
+              href='https://github.com/shingggk/portfolio'
+              className='btn'
+              target='_blank'
+            >
+              Github
+            </a>
+            <a href='' className='btn btn-primary' target='_black'>
+              Live Demo
+            </a>
+          </div>
+        </article>
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'>
+            <img src={PortfolioImg} alt='' />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className='portfolio__item-cta'>
+            <a
+              href='https://github.com/shingggk/portfolio'
+              className='btn'
+              target='_blank'
+            >
+              Github
+            </a>
+            <a href='' className='btn btn-primary' target='_black'>
+              Live Demo
+            </a>
+          </div>
+        </article>
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'>
+            <img src={PortfolioImg} alt='' />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className='portfolio__item-cta'>
+            <a
+              href='https://github.com/shingggk/portfolio'
+              className='btn'
+              target='_blank'
+            >
+              Github
+            </a>
+            <a href='' className='btn btn-primary' target='_black'>
+              Live Demo
+            </a>
+          </div>
+        </article>
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'>
+            <img src={PortfolioImg} alt='' />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className='portfolio__item-cta'>
+            <a
+              href='https://github.com/shingggk/portfolio'
+              className='btn'
+              target='_blank'
+            >
+              Github
+            </a>
+            <a href='' className='btn btn-primary' target='_black'>
+              Live Demo
+            </a>
+          </div>
+        </article>{' '}
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'>
+            <img src={PortfolioImg} alt='' />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className='portfolio__item-cta'>
+            <a
+              href='https://github.com/shingggk/portfolio'
+              className='btn'
+              target='_blank'
+            >
+              Github
+            </a>
+            <a href='' className='btn btn-primary' target='_black'>
+              Live Demo
+            </a>
+          </div>
+        </article>
+      </div>
+    </section>
+  );
 };
 
 export default Works;
