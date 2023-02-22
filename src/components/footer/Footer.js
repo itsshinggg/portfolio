6b09ce42bd2776887b8f5831b309166ccdc88baa
shingggk <kiseshingo@cityuniversity.edu>
2023-02-21 18:19:24 -0800

    more sections add

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,7 +1,24 @@
 import './Footer.css';
 
+import { BsLinkedin } from 'react-icons/bs';
+import { GrGithub } from 'react-icons/gr';
+
 const Footer = () => {
-  return <div></div>;
+  return (
+    <footer>
+      <a href='#' className='footer__name'>
+        Shingo Kise
+      </a>
+      <div className='footer_socials'>
+        <a href='https://www.linkedin.com/in/shingokise' target='_blank'>
+          <BsLinkedin />
+        </a>
+        <a href='https://github.com/shingggk' target='_blank'>
+          <GrGithub />
+        </a>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- src/components/skillsets/Skillsets.js
+++ src/components/skillsets/Skillsets.js
@@ -1,6 +1,7 @@
 import './Skillsets.css';
 import './Skillsets.css';
 import { BsPatchCheckFill } from 'react-icons/bs';
+import AWSBadge from '../../assets/AWS-badge.png';
 
 const Skillsets = () => {
   return (
@@ -22,88 +23,84 @@ const Skillsets = () => {
             <article className='experience__details'>
               <BsPatchCheckFill className='experience__details-icon' />
               <div>
-                <h4>HTML</h4>
+                <h4>CSS</h4>
                 <small className='text-light'>Familiar</small>
               </div>
             </article>
             <article className='experience__details'>
               <BsPatchCheckFill className='experience__details-icon' />
               <div>
-                <h4>HTML</h4>
-                <small className='text-light'>Familiar</small>
-              </div>
-            </article>
-            <article className='experience__details'>
-              <BsPatchCheckFill className='experience__details-icon' />
-              <div>
-                <h4>HTML</h4>
+                <h4>JavaScript</h4>
                 <small className='text-light'>Familiar</small>
               </div>
             </article>
           </div>
         </div>
         <div className='experience__backend'>
-          <h3>Frontend Development</h3>
+          <h3>Backend Development</h3>
           <div className='experience__content'>
             <article className='experience__details'>
               <BsPatchCheckFill className='experience__details-icon' />
               <div>
-                <h4>HTML</h4>
+                <h4>Node.js</h4>
                 <small className='text-light'>Familiar</small>
               </div>
             </article>
             <article className='experience__details'>
               <BsPatchCheckFill className='experience__details-icon' />
               <div>
-                <h4>HTML</h4>
+                <h4>Mongo DB</h4>
                 <small className='text-light'>Familiar</small>
               </div>
             </article>
             <article className='experience__details'>
               <BsPatchCheckFill className='experience__details-icon' />
               <div>
-                <h4>HTML</h4>
-                <small className='text-light'>Familiar</small>
-              </div>
-            </article>
-            <article className='experience__details'>
-              <BsPatchCheckFill className='experience__details-icon' />
-              <div>
-                <h4>HTML</h4>
+                <h4>Dynamo DB</h4>
                 <small className='text-light'>Familiar</small>
               </div>
             </article>
           </div>
         </div>
         <div className='experience__backend'>
-          <h3>Frontend Development</h3>
+          <h3>Frameworks</h3>
           <div className='experience__content'>
             <article className='experience__details'>
               <BsPatchCheckFill className='experience__details-icon' />
               <div>
-                <h4>HTML</h4>
+                <h4>React</h4>
                 <small className='text-light'>Familiar</small>
               </div>
             </article>
+          </div>
+        </div>
+        <div className='experience__backend'>
+          <h3>Certifications</h3>
+          <div className='experience__content'>
+            <div>
+              <article className='experience__details'>
+                <BsPatchCheckFill className='experience__details-icon' />
+                <div>
+                  <h4>AWS Associate Solutions Architect</h4>
+                </div>
+              </article>
+              <img
+                src={AWSBadge}
+                alt='AWS certification badge'
+                className='certification__icons'
+              />
+              <a
+                src='https://www.credly.com/badges/9fa4a9c2-f4b0-4400-8175-bc115722a361?source=linked_in_profile'
+                alt='AWS credential'
+              >
+                Credentials
+              </a>
+            </div>
             <article className='experience__details'>
               <BsPatchCheckFill className='experience__details-icon' />
               <div>
-                <h4>HTML</h4>
-                <small className='text-light'>Familiar</small>
-              </div>
-            </article>
-            <article className='experience__details'>
-              <BsPatchCheckFill className='experience__details-icon' />
-              <div>
-                <h4>HTML</h4>
-                <small className='text-light'>Familiar</small>
-              </div>
-            </article>
-            <article className='experience__details'>
-              <BsPatchCheckFill className='experience__details-icon' />
-              <div>
-                <h4>HTML</h4>
-                <small className='text-light'>Familiar</small>
+                <h4 className='text-light'>Linux+</h4>
+                <small className='text-light'>Currently Studying</small>
               </div>
             </article>
           </div>
